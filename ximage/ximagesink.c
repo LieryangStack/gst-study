dcad2e4acb016ca5a36cd10a56c5c31f898efce2
LieryangStack <sxlisibo@gmail.com>
2023-10-25 11:47:47 +0800

    update

--- ximage/ximagesink.c
+++ ximage/ximagesink.c
@@ -1,3 +1,27 @@
+/**
+
+ * Copyright (C), 2020-2099, Lieryng Tech. Co., Ltd.
+
+ * FileName: ximagepool.c
+
+ * Author:        Version :          Date:
+
+ * Description:     // 模块描述
+
+ * Version:         // 版本信息
+
+ * Function List:   // 主要函数及其功能
+
+    1. -------
+
+ * History:         // 历史修改记录
+
+      <author>  <time>   <version >   <desc>
+
+
+ */
+
+
 #include "config.h"
 
 /* Our interfaces */
